317bd237dd604648c6f3983bff89efa2f9c98245
BenDevman <bendevman@gmail.com>
2024-12-10 16:48:20 +0300

    feat: add formatTime

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -241,8 +241,10 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const min = minutes > 10 ? minutes : String(minutes).padStart(2, '0');
+  const sec = seconds > 10 ? seconds : String(seconds).padStart(2, '0');
+  return `${min}:${sec}`;
 }
 
 /**
