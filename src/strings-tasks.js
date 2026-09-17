b9fa38f940d824664242006a496b1e02f0f22490
bendevman <bendevman@gmail.com>
2026-09-17 14:55:15 +0300

    feat: add repeatString & removeFirstOccurrences

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -130,8 +130,8 @@ function removeTrailingWhitespaces(value) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  return times <= 0 ? '' : str.repeat(times);
 }
 
 /**
@@ -146,8 +146,13 @@ function repeatString(/* str, times */) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  if (str.search(value) >= 0) {
+    return str
+      .substring(0, str.indexOf(value))
+      .concat(str.substring(str.indexOf(value) + value.length, str.length));
+  }
+  return str;
 }
 
 /**
